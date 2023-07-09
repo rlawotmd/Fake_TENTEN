8d65e93ad4b319f0cdf7af86cb925140f2f156d0
haedanui <rhs9705kr@naver.com>
2023-07-09 18:36:10 +0900

    이미지 경로 변경

--- lab project/test.py
+++ lab project/test.py
@@ -20,8 +20,8 @@ window.resizable(False, False)
 
 
 #이미지 삽입
-logo = PhotoImage(file = "sample_logo.png")
-
+logo = PhotoImage(file = "C:/Users/user/Documents/fake_tenten/Fake_TENTEN/image/sample_logo.png")
+##menu = PhotoImage(file = "../image/sample_logo.png") 이게 되야 되는데 흠
 #원본 크기
 ori_logo_width = logo.width()
 ori_logo_height = logo.height()
@@ -38,7 +38,8 @@ L_logo = tk.Label(window, image = logo, width = 400, height = 100) #
 L_logo.place(x = 200, y = 25)
 
 
-menu = PhotoImage(file = "sample_menu.png")
+menu = PhotoImage(file = "C:/Users/user/Documents/fake_tenten/Fake_TENTEN/image/sample_menu.png")
+##menu = PhotoImage(file = "../image/sample_menu.png") 이게 되야 되는데 흠
 ori_menu_width = menu.width()
 ori_menu_height = menu.height()
 new_menu_width = 300
