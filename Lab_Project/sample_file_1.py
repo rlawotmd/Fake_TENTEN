0a8ca4196a0305147659f27b2b76a41759cb5241
haedanui <rhs9705kr@naver.com>
2023-07-17 17:03:57 +0900

    터틀펜 상대경로 수정

---
+++ Lab_Project/sample_file_1.py
@@ -0,0 +1,13 @@
+import turtle
+
+t = turtle.Turtle()
+t.shape("turtle")
+
+for i in range(5000000):
+    t.fd(100)
+    t.rt(90)
+    t.fd(100)
+    t.rt(90)
+    t.fd(100)
+    t.rt(90)
+    t.fd(100)
