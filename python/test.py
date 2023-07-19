e209144b72462aed7d619c065c12f9516b8d3fa3
K-Jun9 <harrykim0624@gmail.com>
2023-07-19 16:07:04 +0900

    이미지 경로변경

--- python/test.py
+++ python/test.py
@@ -1,19 +1,24 @@
+# -*- coding: utf-8 -*-
 import sys
 import pygame
 from pygame.locals import *
 import time
 
+#프래임
 TARGET_FPS = 30
 clock = pygame.time.Clock()
 
+# 색정의
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 WHITE = (255, 255, 255)
 
-pygame.init()
+pygame.init()#라이브러리초기화
+#디스플레이 초기화
 screen = pygame.display.set_mode((600, 400), DOUBLEBUF) 
 pygame.display.set_caption('돌림판')
 
-img = pygame.image.load('images.png')
+#이미지 불러오기
+img = pygame.image.load('./python/images.png')
