a62151b5240db09c2ad768fbf3013823eb69e1ad
haedanui <rhs9705kr@naver.com>
2023-07-28 19:31:18 +0900

    파이썬기반 게임 버튼 연결

--- Lab_Project/main.py
+++ Lab_Project/main.py
@@ -1,13 +1,17 @@
 import tkinter as tk
 from tkinter import *
 import subprocess
+import webbrowser
 
 #게임의 개수
 MAX = 8
 
 #게임 종류
-python_turtle = "./Lab_Project/turtle_pen.py"
-
+python_turtle = "./Lab_Project/turtle_pen.py" # 예시용 터틀펜
+random_wheel = "./game/준구/python/test.py" # 준구 - 돌림판
+Whack_a_Mole = "./game/사눅/두더지잡기/두더지 잡기.html" # 사눅 - 두더지잡기
+Tic_Tac_Toe = "./game/사눅/tic tac toe with ai 2.py" # 사눅 - 틱택토
+R_S_P = "./game/혜연/R.S.P.py" # 혜연 - 가위바위보
 
 window = tk.Tk()
 
@@ -49,19 +53,25 @@ menu = menu.subsample(ori_menu_width // 300, ori_menu_height // 75) #ori_menu_wi
 
 
 
-#게임 호출 함수
+#파이썬 게임 호출 함수
 def call_turtle():
     subprocess.call(["python", python_turtle])
+    
+def call_Tic_Tac_Toe():
+    subprocess.call(["python", Tic_Tac_Toe])
+
+def call_random_wheel():
+    subprocess.call(["python", random_wheel])
 
 
 #게임 실행하는 버튼 생성
 button = tk.Button(window, image = menu, text="터틀 실행", command = call_turtle, width = 340, height = 85)
 button.place(x = 50, y = 150)
 
-button = tk.Button(window, image = menu, text="터틀 실행", command = call_turtle, width = 340, height = 85)
+button = tk.Button(window, image = menu, text="틱택토 실행", command = call_Tic_Tac_Toe, width = 340, height = 85)
 button.place(x = 410, y = 150)
 
-button = tk.Button(window, image = menu, text="터틀 실행", command = call_turtle, width = 340, height = 85)
+button = tk.Button(window, image = menu, text="돌려돌려 돌림판 실행", command = call_random_wheel, width = 340, height = 85)
 button.place(x = 50, y = 255)
 
 button = tk.Button(window, image = menu, text="터틀 실행", command = call_turtle, width = 340, height = 85)
