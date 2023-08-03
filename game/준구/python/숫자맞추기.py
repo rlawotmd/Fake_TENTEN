2ce5ef0f0d5e651385ca4f29f4712602bd2e7c12
K-Jun9 <harrykim0624@gmail.com>
2023-08-03 17:55:29 +0900

    업다운 최최종

--- game/준구/python/숫자맞추기.py
+++ game/준구/python/숫자맞추기.py
@@ -3,17 +3,20 @@ import random as rd
 num = rd.randint(1,100)
 n = 0
 bool = False
-answer = int(input('1~99사이의 정수를 입력해주세요: '))
 
-for i in range(6):
+
+for i in range(7):
+    answer = int(input('1~99사이의 정수를 입력해주세요: '))
+    if 6-n == 0:
+        print('끝')
+        break
+    print(6-n,'번남았습니다.')
     if answer > num:
         print('DOWN')
-        answer = int(input('1~99사이의 정수를 입력하세요: '))
         n = n+1
 
     elif answer < num:
         print('UP')
-        answer = int(input('1~99사이의 정수를 입력하세요: '))
         n = n+1
 
     else:
@@ -23,7 +26,7 @@ for i in range(6):
         break
 
 if bool == False:
-    print('아쉽게도 못맞췄습니다. 다음 기회에~~')
+    print('아쉽게도 못맞췄습니다.정답은',num , '였습니다.')
 
-
+print('<아무키나 눌러서 게임종료.>')
 input()
