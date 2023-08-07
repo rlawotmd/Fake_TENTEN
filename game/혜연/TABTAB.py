d7c61e291a3c303757d8310f264ba5c17a636fbb
bebeghi <star0556@naver.com>
2023-08-07 14:49:52 +0900

    수정

--- game/혜연/TABTAB.py
+++ game/혜연/TABTAB.py
@@ -1,11 +1,40 @@
 import turtle as t
-import random
 import pygame
+from turtle import back
 import time
 
+pygame.init()
+
+RED = (255, 0,0)
+GREEN = (0, 255, 0)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+'''background = pygame.display.set_mode((480, 360))
+pygame.display.set_caption("text")
+
+myFont = pygame.font.SysFont(None, 50)'''
+
+
+play = True
+while play:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            play = False
+
+    background.fill((0,0,0))
+    myText = myFont.render("10sec", True, WHITE)
+    background.blit(myText, (100,100))
+    pygame.display.update()
+
+
 def count_clicks_within_time_limit(time_limit):
-    pygame.init()
-    screen = pygame.display.set_mode((500, 700))
+    screen_width = 500
+    screen_height = 700 
+
+    screen = pygame.display.set_mode((screen_width, screen_height))
+    pygame.display.set_caption("TOTAL SCORE")
+
     click_count = 0
     start_time = time.time()
 
@@ -16,7 +45,25 @@ def count_clicks_within_time_limit(time_limit):
 
 
         pygame.display.flip()
+  
+    #clickScore = 0
+    tabtab_font = pygame.font.SysFont("arialrounded", 40, True, False)
+    text_clickScore = tabtab_font.render("TOTAL SCORE", True, WHITE)
+    text_Rect = text_clickScore.get_rect() 
+    text_Rect.centerx = round(screen_width /2)
+    text_Rect.y = 50
+    #클릭 기록 창이 꺼져야 게임 결과창이 뜨나
+    #터틀로 텍스트(
 
+
+    '''screen.blit(text_clickScore, text_Rect)
+
+    text_Total = tabtab_font.render("Total Score", True, WHITE)
+    screen.blit(text_Total, [100, 400])'''
+
+    pygame.display.flip()
+
+    time.sleep(5)
     pygame.quit()
     return click_count
 
@@ -25,9 +72,4 @@ if __name__ == "__main__":
 
     print(f"터치 횟수 계산 중... (제한 시간: {time_limit_seconds}초)")
     click_count_result = count_clicks_within_time_limit(time_limit_seconds)
-    #print(f"{time_count_result}")
-
-
-#터치 횟수 안 뜸
-#time_count_result 변수 쓰임 찾기
-#push test
+    print(f"{click_count_result}")
